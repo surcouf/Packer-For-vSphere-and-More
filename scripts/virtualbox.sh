1822b37b731d211190e957498d1dcd25528df247
Raphaël SurcouF <surcouf@debianfr.net>
2018-05-08 12:23:01 +0200

    scripts/virtualbox: install VirtualBox guest additions tools with official RPM instead of ISO image.

--- scripts/virtualbox.sh
+++ scripts/virtualbox.sh
@@ -24,11 +24,6 @@ fi
 
 # RHEL
 if [ -f /etc/redhat-release ]; then
-  sudo yum -y install gcc kernel-devel kernel-headers dkms make bzip2 perl && \
-  sudo yum -y groupinstall "Development Tools"
-  sudo mkdir -p /mnt/virtualbox
-  sudo mount -o loop /home/packer/VBoxGuestAdditions.iso /mnt/virtualbox
-  sudo sh /mnt/virtualbox/VBoxLinuxAdditions.run
-  sudo umount /mnt/virtualbox
-  sudo rm -rf /home/packer/VBoxGuestAdditions.iso
+  sudo curl -fSs -o VirtualBox-5.2-5.2.10_122088_el6-1.x86_64.rpm http://download.virtualbox.org/virtualbox/rpm/rhel/6/x86_64/VirtualBox-5.2-5.2.10_122088_el6-1.x86_64.rpm
+  sudo yum localinstall -y VirtualBox-5.2-5.2.10_122088_el6-1.x86_64.rpm
 fi
